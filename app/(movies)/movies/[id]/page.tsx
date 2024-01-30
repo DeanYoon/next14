20ec31f8f4a8cc539b2412017456dd98a96602b3
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2024-01-30 16:40:30 +0900

    feat: css added

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -17,11 +17,10 @@ export async function generateMetadata({ params: { id } }: IParams) {
 export default async function MovieDetail({ params: { id } }: IParams) {
   return (
     <div>
-      <h3>Movie detail page</h3>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
-      <h4>Videos</h4>
+
       <Suspense fallback={<h1>Loading movie videos</h1>}>
         <MovieVideos id={id} />
       </Suspense>
--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { API_URL } from "../app/constants";
-
+import style from "../styles/movie-info.module.css";
 export async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
   const json = await response.json();
@@ -9,5 +9,17 @@ export async function getMovie(id: string) {
 
 export async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={style.container}>
+      <img src={movie.poster_path} className={style.poster} alt={movie.title} />
+      <div className={style.info}>
+        <h1 className={style.title}>{movie.title}</h1>
+        <h3>⭐️ {movie.vote_average.toFixed()}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={"_blank"}>
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
